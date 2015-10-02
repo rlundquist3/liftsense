94eaedea81c4eeeff34e28087fe528abdd937383
Riley Lundquist <rlundquist3@gmail.com>
2015-10-02 12:52:53 -0400

    added color detect files

---
+++ app/src/main/java/com/rileylundquist/liftsense/WorkoutContent.java
@@ -0,0 +1,55 @@
+package com.rileylundquist.liftsense;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p/>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class WorkoutContent {
+
+    /**
+     * An array of sample (dummy) items.
+     */
+    public static List<Exercise> ITEMS = new ArrayList<Exercise>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static Map<String, Exercise> ITEM_MAP = new HashMap<String, Exercise>();
+
+    /*static {
+        // Add 3 sample items.
+        addItem(new Exercise("1", "Item 1"));
+        addItem(new Exercise("2", "Item 2"));
+        addItem(new Exercise("3", "Item 3"));
+    }*/
+
+    public static void addItem(Exercise item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public static class Exercise {
+        public String id;
+        public String content;
+
+        public Exercise(String id, String content) {
+            this.id = id;
+            this.content = content;
+        }
+
+        @Override
+        public String toString() {
+            return content;
+        }
+    }
+}
---
+++ app/src/main/jni/Object.cpp
@@ -0,0 +1,121 @@
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software")
+//, to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+//IN THE SOFTWARE.
+
+#include "Object.h"
+
+Object::Object()
+{
+	//set values for default constructor
+	setType("Object");
+	setColor(Scalar(0,0,0));
+
+}
+
+Object::Object(string name){
+
+	setType(name);
+	
+	if(name=="blue"){
+
+		//TODO: use "calibration mode" to find HSV min
+		//and HSV max values
+
+		setHSVmin(Scalar(92,0,0));
+		setHSVmax(Scalar(124,256,256));
+
+		//BGR value for Green:
+		setColor(Scalar(255,0,0));
+
+	}
+	if(name=="green"){
+
+		//TODO: use "calibration mode" to find HSV min
+		//and HSV max values
+
+		setHSVmin(Scalar(34,50,50));
+		setHSVmax(Scalar(80,220,200));
+
+		//BGR value for Yellow:
+		setColor(Scalar(0,255,0));
+
+	}
+	if(name=="yellow"){
+
+		//TODO: use "calibration mode" to find HSV min
+		//and HSV max values
+
+		setHSVmin(Scalar(20,124,123));
+		setHSVmax(Scalar(30,256,256));
+
+		//BGR value for Red:
+		setColor(Scalar(0,255,255));
+
+	}
+	if(name=="red"){
+
+		//TODO: use "calibration mode" to find HSV min
+		//and HSV max values
+
+		setHSVmin(Scalar(0,200,0));
+		setHSVmax(Scalar(19,255,255));
+
+		//BGR value for Red:
+		setColor(Scalar(0,0,255));
+
+	}
+}
+
+Object::~Object(void)
+{
+}
+
+int Object::getXPos(){
+
+	return Object::xPos;
+
+}
+
+void Object::setXPos(int x){
+
+	Object::xPos = x;
+
+}
+
+int Object::getYPos(){
+
+	return Object::yPos;
+
+}
+
+void Object::setYPos(int y){
+
+	Object::yPos = y;
+
+}
+
+Scalar Object::getHSVmin(){
+
+	return Object::HSVmin;
+
+}
+Scalar Object::getHSVmax(){
+
+	return Object::HSVmax;
+}
+
+void Object::setHSVmin(Scalar min){
+
+	Object::HSVmin = min;
+}
+
+
+void Object::setHSVmax(Scalar max){
+
+	Object::HSVmax = max;
+}
