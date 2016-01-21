79f4c14850daae5ae62e9f508e4621c405ec0514
Riley Lundquist <rlundquist3@gmail.com>
2016-01-21 16:26:09 -0500

    added camera header file

---
+++ app/src/main/jni/com_rileylundquist_liftsense_CameraActivity.h
@@ -0,0 +1,17 @@
+#include <jni.h>
+/* Header for class com_rileylundquist_liftsense_CameraActivity */
+
+#ifndef _Included_com_rileylundquist_liftsense_CameraActivity
+#define _Included_com_rileylundquist_liftsense_CameraActivity
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT void JNICALL Java_com_rileylundquist_liftsense_CameraActivity_nativeColorDetect
+(JNIEnv *, jobject, jlong);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
