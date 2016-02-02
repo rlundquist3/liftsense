a45f2b556462df7806f05ee3b6500564392a8550
Liam Lundy <liam.lundy13@kzoo.edu>
2016-02-02 11:55:04 -0500

    new experimental gradle works but doesn't include opencv yet

---
+++ app/src/main/jni/MultiObjectTrackingBasedOnColor/MultipleObjectTracking.cpp
@@ -0,0 +1,269 @@
+//Written by  Kyle Hounslow 2013
+
+// modified by: Ahmad Kaifi, Hassan Althobaiti
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software")
+//, to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+//IN THE SOFTWARE.
+
+/*#include <sstream>
+#include <string>
+#include <iostream>
+#include <vector>
+
+#include "Object.h"*/
+
+#include "MultipleObjectTracking.h"
+
+
+//initial min and max HSV filter values.
+//these will be changed using trackbars
+int H_MIN = 0;
+int H_MAX = 256;
+int S_MIN = 0;
+int S_MAX = 256;
+int V_MIN = 0;
+int V_MAX = 256;
+//default capture width and height
+const int FRAME_WIDTH = 640;
+const int FRAME_HEIGHT = 480;
+//max number of objects to be detected in frame
+const int MAX_NUM_OBJECTS=50;
+//minimum and maximum object area
+const int MIN_OBJECT_AREA = 20*20;
+const int MAX_OBJECT_AREA = FRAME_HEIGHT*FRAME_WIDTH/1.5;
+//names that will appear at the top of each window
+const string windowName = "Original Image";
+const string windowName1 = "HSV Image";
+const string windowName2 = "Thresholded Image";
+const string windowName3 = "After Morphological Operations";
+const string trackbarWindowName = "Trackbars";
+
+//The following for canny edge detec
+Mat dst, detected_edges;
+Mat src, src_gray;
+int edgeThresh = 1;
+int lowThreshold;
+int const max_lowThreshold = 100;
+int ratio = 3;
+int kernel_size = 3;
+char* window_name = "Edge Map";
+
+
+string MultipleObjectTracking::intToString(int number){
+
+	std::stringstream ss;
+	ss << number;
+	return ss.str();
+}
+
+void MultipleObjectTracking::drawObject(vector<Object> theObjects,Mat &frame, Mat &temp, vector<vector<Point> > contours, vector<Vec4i> hierarchy){
+
+	for(int i =0; i<theObjects.size(); i++){
+	cv::drawContours(frame,contours,i,theObjects.at(i).getColor(),3,8,hierarchy);
+	cv::circle(frame,cv::Point(theObjects.at(i).getXPos(),theObjects.at(i).getYPos()),5,theObjects.at(i).getColor());
+	cv::putText(frame,intToString(theObjects.at(i).getXPos())+ " , " + intToString(theObjects.at(i).getYPos()),cv::Point(theObjects.at(i).getXPos(),theObjects.at(i).getYPos()+20),1,1,theObjects.at(i).getColor());
+	cv::putText(frame,theObjects.at(i).getType(),cv::Point(theObjects.at(i).getXPos(),theObjects.at(i).getYPos()-20),1,2,theObjects.at(i).getColor());
+	}
+}
+
+void MultipleObjectTracking::drawObject(vector<Object> theObjects,Mat &frame){
+
+	for(int i =0; i<theObjects.size(); i++){
+
+	cv::circle(frame,cv::Point(theObjects.at(i).getXPos(),theObjects.at(i).getYPos()),10,cv::Scalar(0,0,255));
+	cv::putText(frame,intToString(theObjects.at(i).getXPos())+ " , " + intToString(theObjects.at(i).getYPos()),cv::Point(theObjects.at(i).getXPos(),theObjects.at(i).getYPos()+20),1,1,Scalar(0,255,0));
+	cv::putText(frame,theObjects.at(i).getType(),cv::Point(theObjects.at(i).getXPos(),theObjects.at(i).getYPos()-30),1,2,theObjects.at(i).getColor());
+	}
+}
+
+void MultipleObjectTracking::morphOps(Mat &thresh){
+
+	//create structuring element that will be used to "dilate" and "erode" image.
+	//the element chosen here is a 3px by 3px rectangle
+	Mat erodeElement = getStructuringElement( MORPH_RECT,Size(3,3));
+	//dilate with larger element so make sure object is nicely visible
+	Mat dilateElement = getStructuringElement( MORPH_RECT,Size(8,8));
+
+	erode(thresh,thresh,erodeElement);
+	erode(thresh,thresh,erodeElement);
+
+	dilate(thresh,thresh,dilateElement);
+	dilate(thresh,thresh,dilateElement);
+}
+
+void MultipleObjectTracking::trackFilteredObject(Mat threshold,Mat HSV, Mat &cameraFeed)
+{
+	vector <Object> objects;
+	Mat temp;
+	threshold.copyTo(temp);
+	//these two vectors needed for output of findContours
+	vector< vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	//find contours of filtered image using openCV findContours function
+	findContours(temp,contours,hierarchy,CV_RETR_CCOMP,CV_CHAIN_APPROX_SIMPLE );
+	//use moments method to find our filtered object
+	double refArea = 0;
+	bool objectFound = false;
+	if (hierarchy.size() > 0) {
+		int numObjects = hierarchy.size();
+		//if number of objects greater than MAX_NUM_OBJECTS we have a noisy filter
+		if(numObjects<MAX_NUM_OBJECTS)
+		{
+			for (int index = 0; index >= 0; index = hierarchy[index][0])
+			{
+				Moments moment = moments((cv::Mat)contours[index]);
+				double area = moment.m00;
+				//if the area is less than 20 px by 20px then it is probably just noise
+				//if the area is the same as the 3/2 of the image size, probably just a bad filter
+				//we only want the object with the largest area so we safe a reference area each
+				//iteration and compare it to the area in the next iteration.
+				if(area>MIN_OBJECT_AREA)
+				{
+					Object object;
+
+					object.setXPos(moment.m10/area);
+					object.setYPos(moment.m01/area);
+
+					objects.push_back(object);
+
+					objectFound = true;
+
+				}
+				else objectFound = false;
+			}
+			//let user know you found an object
+			if(objectFound ==true)
+			{
+				//draw object location on screen
+				drawObject(objects,cameraFeed);
+			}
+		}
+		else putText(cameraFeed,"TOO MUCH NOISE! ADJUST FILTER",Point(0,50),1,2,Scalar(0,0,255),2);
+	}
+}
+
+void MultipleObjectTracking::trackFilteredObject(Object theObject,Mat threshold,Mat HSV, Mat &cameraFeed){
+
+	vector <Object> objects;
+	Mat temp;
+	threshold.copyTo(temp);
+	//these two vectors needed for output of findContours
+	vector< vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	//find contours of filtered image using openCV findContours function
+	findContours(temp,contours,hierarchy,CV_RETR_CCOMP,CV_CHAIN_APPROX_SIMPLE );
+	//use moments method to find our filtered object
+	double refArea = 0;
+	bool objectFound = false;
+	if (hierarchy.size() > 0) {
+		int numObjects = hierarchy.size();
+		//if number of objects greater than MAX_NUM_OBJECTS we have a noisy filter
+		if(numObjects<MAX_NUM_OBJECTS){
+			for (int index = 0; index >= 0; index = hierarchy[index][0]) {
+
+				Moments moment = moments((cv::Mat)contours[index]);
+				double area = moment.m00;
+
+		//if the area is less than 20 px by 20px then it is probably just noise
+		//if the area is the same as the 3/2 of the image size, probably just a bad filter
+		//we only want the object with the largest area so we safe a reference area each
+				//iteration and compare it to the area in the next iteration.
+				if(area>MIN_OBJECT_AREA){
+
+					Object object;
+
+					object.setXPos(moment.m10/area);
+					object.setYPos(moment.m01/area);
+					object.setType(theObject.getType());
+					object.setColor(theObject.getColor());
+
+					objects.push_back(object);
+
+					objectFound = true;
+
+				}else objectFound = false;
+			}
+			//let user know you found an object
+			if(objectFound ==true){
+				//draw object location on screen
+				drawObject(objects,cameraFeed,temp,contours,hierarchy);}
+
+		}else putText(cameraFeed,"TOO MUCH NOISE! ADJUST FILTER",Point(0,50),1,2,Scalar(0,0,255),2);
+	}
+}
+
+/**
+ * Formerly main--changed to be function called by main.cpp
+ */
+//int main(int argc, char* argv[])
+Mat MultipleObjectTracking::detect(jlong imageRgba)
+{
+	bool calibrationMode = false;
+
+	//Matrix to store each frame of the webcam feed
+	Mat& cameraFeed = *(Mat*) imageRgba;
+	Mat threshold;
+	Mat HSV;
+
+	//convert frame from BGR to HSV colorspace
+	cvtColor(cameraFeed,HSV,COLOR_BGR2HSV);
+
+	if(calibrationMode==true){
+
+	//need to find the appropriate color range values
+	// calibrationMode must be false
+
+	//if in calibration mode, we track objects based on the HSV slider values.
+		cvtColor(cameraFeed,HSV,COLOR_BGR2HSV);
+		inRange(HSV,Scalar(H_MIN,S_MIN,V_MIN),Scalar(H_MAX,S_MAX,V_MAX),threshold);
+		morphOps(threshold);
+		imshow(windowName2,threshold);
+
+	//the folowing for canny edge detec
+		/// Create a matrix of the same type and size as src (for dst)
+		dst.create( src.size(), src.type() );
+		/// Convert the image to grayscale
+		cvtColor( src, src_gray, CV_BGR2GRAY );
+		/// Create a window
+		namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+		/// Create a Trackbar for user to enter threshold
+		createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold);
+		/// Show the image
+		trackFilteredObject(threshold,HSV,cameraFeed);
+	}
+	else{
+		//create some temp fruit objects so that
+		//we can use their member functions/information
+		Object blue("blue"), yellow("yellow"), red("red"), green("green");
+
+		//first find blue objects
+		cvtColor(cameraFeed,HSV,COLOR_BGR2HSV);
+		inRange(HSV,blue.getHSVmin(),blue.getHSVmax(),threshold);
+		morphOps(threshold);
+		trackFilteredObject(blue,threshold,HSV,cameraFeed);
+		//then yellows
+		cvtColor(cameraFeed,HSV,COLOR_BGR2HSV);
+		inRange(HSV,yellow.getHSVmin(),yellow.getHSVmax(),threshold);
+		morphOps(threshold);
+		trackFilteredObject(yellow,threshold,HSV,cameraFeed);
+		//then reds
+		cvtColor(cameraFeed,HSV,COLOR_BGR2HSV);
+		inRange(HSV,red.getHSVmin(),red.getHSVmax(),threshold);
+		morphOps(threshold);
+		trackFilteredObject(red,threshold,HSV,cameraFeed);
+		//then greens
+		cvtColor(cameraFeed,HSV,COLOR_BGR2HSV);
+		inRange(HSV,green.getHSVmin(),green.getHSVmax(),threshold);
+		morphOps(threshold);
+		trackFilteredObject(green,threshold,HSV,cameraFeed);
+
+	}
+
+	return cameraFeed;
+}
